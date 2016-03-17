72782519491e3ddd2828b66f08e487e940e3c8a0
LeeBry <Echoslee@yahoo.com>
2016-03-17 14:14:16 -0500

    Everything is good, now just figure out the equation now
    angles :o:

--- FractalTreeLab/TreePanel.java
+++ FractalTreeLab/TreePanel.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import javax.swing.JPanel;
+import java.awt.geom.Line2D;
 /**
  * Write a description of class TreePanel here.
  * 
@@ -11,49 +12,44 @@ public class TreePanel extends JPanel
     private final int PANEL_WIDTH= 500;
     private final int PANEL_HEIGHT=500;
     
-    private final int x1=100;
-    private final int y1=100;
+    private int x1;
+    private int y1;
     // points for the right branch
-    private final int x2=100;
-    private final int y3=100; 
-    // points for the left branch
-    private int originX;
-    private int originY;
-    // Origin of the original branch
+    private int x2;
+    private int y2; 
+    // points for the left branch    
     private Color branchColor;
     // Color of the branch
     private int branchLength= 100;
     // This is the branch level
-    private double angle;
+    private double branchAngle = 50;
+    private double rAngle;
+    private double lAngle;
     // angle
-    private double factor;
+    private double factorLength;
     /**
      * Constructor for objects of class TreePanel
      */
     public TreePanel(int branch)
     {
-      branchLength = branch;
-      //setBackground (Color.green);
+      this.branchLength = branch;
+      this.branchAngle= branchAngle;
+      this.factorLength= 0.8;
+      setBackground (Color.white);
       setPreferredSize (new Dimension(PANEL_WIDTH, PANEL_HEIGHT));   
     }
-    
-    public void paintComponent(Graphics g2)
-    {
-        super.paintComponent(g2);
 
-    }
-    
-    public void drawFractal (int originX, int originY, int length,
-                             double angle, Graphics2D g2)
+    public void drawFractal (int x1, int y1, int length,double angle, Graphics2D page)
     {
       int lengthSin, lengthCos;
-      lengthSin= (int)Math.sin(angle*Math.PI/180);
-      lengthSin= lengthSin*length;
-      lengthCos= (int)Math.cos(angle*Math.PI/180);
-      lengthCos= lengthCos*length;
+      lAngle = branchAngle+ lAngle;
+      rAngle = branchAngle- rAngle;
+      
+      lengthSin= length*(int)Math.sin(angle*Math.PI/180);
+      lengthCos= length*(int)Math.cos(angle*Math.PI/180);
       if (true && length>15)
       {
-          g2.drawLine(originX,originY,0,length);
+          page.draw(new Line2D.Double (x1,y1,x2,y2));
       }
       else
       {
@@ -61,5 +57,17 @@ public class TreePanel extends JPanel
      
       }
     }
+        
+    public void paintComponent(Graphics page)
+    {
+        Graphics2D g2= (Graphics2D) page;
+        super.paintComponent(page);
+        g2.drawLine(250, 250, 250, 500);
+        
+        drawFractal(x1, y1, branchLength, rAngle, g2);
+        drawFractal(x1, y1, branchLength, lAngle, g2);
+
+    }
+    
 
 }
