0acf976a79410ef3f60c89d4a52b60b2ee9cb79e
LeeBry <Echoslee@yahoo.com>
2016-03-18 07:48:07 -0500

    work in progress
    Don't use instance variables in the drawFractal method!

--- FractalTreeLab/TreePanel.java
+++ FractalTreeLab/TreePanel.java
@@ -12,19 +12,17 @@ public class TreePanel extends JPanel
     private final int PANEL_WIDTH= 500;
     private final int PANEL_HEIGHT=500;
     
-    private int x1;
-    private int y1;
-    // points for the right branch
+    private int x1=250;
+    private int y1=250;
     private int x2;
     private int y2; 
-    // points for the left branch    
+    // points for the left and right branch    
     private Color branchColor;
     // Color of the branch
-    private int branchLength= 100;
+    private double branchLength= 100;
     // This is the branch level
     private double branchAngle = 50;
-    private double rAngle;
-    private double lAngle;
+    private double rAngle= 10, lAngle = 10;
     // angle
     private double factorLength;
     /**
@@ -42,19 +40,32 @@ public class TreePanel extends JPanel
     public void drawFractal (int x1, int y1, int length,double angle, Graphics2D page)
     {
       int lengthSin, lengthCos;
+      // Dont use instance variables. Use the parameter values
       lAngle = branchAngle+ lAngle;
       rAngle = branchAngle- rAngle;
       
-      lengthSin= length*(int)Math.sin(angle*Math.PI/180);
-      lengthCos= length*(int)Math.cos(angle*Math.PI/180);
-      if (true && length>15)
+
+      if (true && branchLength>15)
       {
           page.draw(new Line2D.Double (x1,y1,x2,y2));
       }
       else
       {
-         // call drawFractal, and drawLine twice for left and right side
-     
+          this.branchLength = (double)this.branchLength*factorLength;
+          lengthSin= length*(int)Math.sin(lAngle*Math.PI/180);
+          lengthCos= length*(int)Math.cos(lAngle*Math.PI/180);
+          x2= x1-lengthSin;
+          y2= y1-lengthCos;
+          page.draw(new Line2D.Double (x1,y1,x2,y2));
+          drawFractal(x2, y2, (int)branchLength, lAngle, page);
+          
+          lengthSin= length*(int)Math.sin(rAngle*Math.PI/180);
+          lengthCos= length*(int)Math.cos(rAngle*Math.PI/180);
+          x2= x1+lengthSin;
+          y2= y1-lengthCos;
+          page.draw(new Line2D.Double (x1,y1,x2,y2));
+          drawFractal(x2, y2, (int)branchLength, rAngle, page);
+         
       }
     }
         
@@ -62,10 +73,9 @@ public class TreePanel extends JPanel
     {
         Graphics2D g2= (Graphics2D) page;
         super.paintComponent(page);
-        g2.drawLine(250, 250, 250, 500);
+        g2.drawLine(250, 250, 250, 500); //draws the trunk of the tree
         
-        drawFractal(x1, y1, branchLength, rAngle, g2);
-        drawFractal(x1, y1, branchLength, lAngle, g2);
+        drawFractal(x1, y1, (int)branchLength, rAngle, g2);
 
     }
     
