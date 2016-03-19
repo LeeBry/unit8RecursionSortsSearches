5d11c2dc04ade12e8da92dcd8a4102039f55201e
LeeBry <Echoslee@yahoo.com>
2016-03-19 18:16:22 -0500

    Getting somewhere :o
    Working... kinda

--- FractalTreeLab/TreePanel.java
+++ FractalTreeLab/TreePanel.java
@@ -11,70 +11,59 @@ public class TreePanel extends JPanel
 {
     private final int PANEL_WIDTH= 500;
     private final int PANEL_HEIGHT=500;
-
-    private int x1=250;
-    private int y1=250;
-    private int x2;
-    private int y2; 
-    // points for the left and right branch    
+    private int startX= 250;
+    private int startY=500;
     private Color branchColor;
-    // Color of the branch
     private double branchLength;
-    // This is the branch level
     private double branchAngle = 50;
-    private double rAngle, lAngle;
-    // angle
+    private double angleChange= 5;
     private double factorLength;
     /**
      * Constructor for objects of class TreePanel
      */
     public TreePanel(double branch)
     {
-        System.out.println("This is inside the TreePanel");
         this.branchLength = branch;
-        System.out.println("branchLength: "+ branch);
         this.branchAngle= branchAngle;
-        System.out.println("branchAngle: "+branchAngle);
         this.factorLength= 0.8;
-        System.out.println("FactorLength: "+factorLength);
-        this.rAngle= 10;
-        this.lAngle= 10;
-        System.out.println("rAngle and lAngle"+ rAngle+""+lAngle);
+        //Inside constructor: branchLength= 100, branchAngle= 50, factorLength= 0.8, l & rAngle= 0.0
         setBackground (Color.white);
         setPreferredSize (new Dimension(PANEL_WIDTH, PANEL_HEIGHT));   
     }
 
     public void drawFractal (int x1, int y1, int length,double angle, Graphics2D page)
     {
-        // int lengthSin, lengthCos;
         // Dont use instance variables. Use the parameter values
-         lAngle = branchAngle+ this.lAngle;
-         rAngle = branchAngle- this.rAngle;
-        // something is wrong with the angle. *******
+        int startX=x1, startY=y1, newLength=length, deltaX, deltaY;
+        double newAngle=angle;
+        double lAngle, rAngle;
+        // 1st run: startX= 250, startY= 500, Length= 100, angle=50
+        
         if (true && branchLength<15)
         {
-            page.draw(new Line2D.Double (x1,y1,x2,y2));
+            //page.draw(new Line2D.Double (startX,startY,startX,startY+50));
         }
         else
         {
             this.branchLength = (int)this.branchLength*factorLength;
+            this.branchLength= (int)branchLength;
             System.out.println("branchLength inside draw:"+branchLength);
-            System.out.println("lAngle and rAngle :"+ lAngle+" "+rAngle);
-            int x2= x1-length*(int)Math.sin(lAngle);
-            int y2= y1-length*(int)Math.cos(lAngle);
-
-            System.out.println("x2 y2 inside draw:"+ x2+" "+y2);
-
-            page.draw(new Line2D.Double (x1,y1,x2,y2));
-
-            drawFractal(x2, y2, (int)branchLength, lAngle, page);
-
-            int x3= x1+length*(int)Math.sin(rAngle*Math.PI/180);
-            int y3= y2-length*(int)Math.cos(rAngle*Math.PI/180);
-
-            page.draw(new Line2D.Double (x1,y1,x3,y3));
-            drawFractal(x3, y3, (int)branchLength, rAngle, page);
-
+            // this sets the new branch Length
+            lAngle = newLength*Math.sin(Math.toRadians(newAngle)); //new X
+            rAngle = newLength*Math.cos(Math.toRadians(newAngle)); // new Y
+            // Then convert to int version to find the new X and Y
+            deltaX = startX-(int)lAngle;
+            deltaY = startY-(int)rAngle;
+            System.out.println("L R"+lAngle+" "+rAngle);
+            System.out.println("X  Y"+deltaX+" "+deltaY);
+            page.draw(new Line2D.Double(startX,0, deltaX, deltaY));
+//             deltaX= startX-(length*(int)Math.sin(newAngle));
+//             deltaY= startY-(length*(int)Math.cos(newAngle));
+//             System.out.println("startX startY inside draw:"+ startX+" "+startY);
+//             System.out.println("deltaX Y inside draw:"+ deltaX+" "+deltaY);
+//             page.draw(new Line2D.Double (x1,y1,startX,startY));
+//             
+//             drawFractal(startX-deltaX, startY-deltaY, (int)branchLength, newAngle, page);
         }
     }
 
@@ -82,9 +71,9 @@ public class TreePanel extends JPanel
     {
         Graphics2D g2= (Graphics2D) page;
         super.paintComponent(page);
-        g2.drawLine(x1, y1, x1, y1+100); //draws the trunk of the tree
-
-        drawFractal(x1, y1, (int)branchLength, rAngle, g2);
+        g2.drawLine(startX, 250, 250, startY); //draws the trunk of the tree
+        branchColor = new Color(0,0,255);
+        drawFractal(startX, startY, (int)branchLength, branchAngle, g2);
 
     }
 
